71d536dfdeba47532d4890339ce6993569925cfd
skarltjr <kisa0828@naver.com>
2023-02-01 15:16:05 +0900

    basic settings

--- src/main/java/exception/BaseballException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class BaseballException extends RuntimeException {
-    public BaseballException(ExceptionStatus status) {
-        super(status.getMessage());
-    }
-}
